35144bfe4c045fcf7b01303407d9f9f962eed581
Antonio Rito Silva <Rito.Silva@ist.utl.pt>
2019-02-14 11:19:14 +0000

    fixes

--- car/src/test/groovy/pt/ulisboa/tecnico/softeng/car/domain/RentingConstructorSpockTest.groovy
+++ car/src/test/groovy/pt/ulisboa/tecnico/softeng/car/domain/RentingConstructorSpockTest.groovy
@@ -2,87 +2,54 @@ package pt.ulisboa.tecnico.softeng.car.domain
 
 import org.joda.time.LocalDate
 import pt.ulisboa.tecnico.softeng.car.exception.CarException
+import spock.lang.Shared
+import spock.lang.Unroll
+
+import java.sql.Driver
 
 class RentingConstructorSpockTest extends SpockRollbackTestAbstractClass {
 
-    private static final String RENT_A_CAR_NAME = 'Eartz'
-    private static final String PLATE_CAR = '22-33-HZ'
-    private static final String DRIVING_LICENSE = 'br112233'
-    private static final LocalDate date1 = LocalDate.parse('2018-01-06')
-    private static final LocalDate date2 = LocalDate.parse('2018-01-07')
-    private static final String NIF = 'NIF'
-    private static final String IBAN = 'IBAN'
-    private static final String IBAN_BUYER = 'IBAN'
-    private Car car
+     def RENT_A_CAR_NAME = 'Eartz'
+     def PLATE_CAR = '22-33-HZ'
+     @Shared def DRIVING_LICENSE = 'br112233'
+     @Shared def date1 = LocalDate.parse('2018-01-06')
+     @Shared def date2 = LocalDate.parse('2018-01-07')
+     @Shared def NIF = 'NIF'
+     @Shared def IBAN = 'IBAN'
+     @Shared def IBAN_BUYER = 'IBAN'
+     @Shared def car
 
     @Override
     def populate4Test() {
-        RentACar rentACar = new RentACar(RENT_A_CAR_NAME,NIF,IBAN)
-        car=new Car(PLATE_CAR,10,10,rentACar)
+        RentACar rentACar = new RentACar(RENT_A_CAR_NAME, NIF,IBAN)
+        car = new Car(PLATE_CAR, 10, 10, rentACar)
     }
 
     def 'success'() {
-        given:
-        Renting renting=new Renting(DRIVING_LICENSE,date1,date2,this.car,NIF,IBAN_BUYER)
+        when:
+        Renting renting = new Renting(DRIVING_LICENSE, date1, date2, car, NIF, IBAN_BUYER)
 
-        expect:
+        then:
         renting.getDrivingLicense() == DRIVING_LICENSE
-        this.car.getPrice() * (date2.getDayOfYear() - date1.getDayOfYear() + 1) == renting.getPrice()
+        car.getPrice() * (date2.getDayOfYear() - date1.getDayOfYear() + 1) == renting.getPrice()
     }
 
-    def 'null driving license'() {
-        when:
-        new Renting(null,date1,date2,this.car,NIF,IBAN_BUYER)
+    @Unroll('RentACar: #dl | #d1 | #d2 | #veh | #nif | #iban')
+    def 'exceptions'() {
+        when: 'renting with wrong arguments'
+        new Renting(dl, d1, d2, veh, nif, iban)
 
-        then:
+        then: 'throws an exception'
         thrown(CarException)
-    }
 
-    def 'empty driving license'() {
-        when:
-        new Renting('',date1,date2,this.car,NIF,IBAN_BUYER)
-
-        then:
-        thrown(CarException)
-    }
-
-    def 'invalid driving license'() {
-        when:
-        new Renting('12',date1,date2,this.car,NIF,IBAN_BUYER)
-
-        then:
-        thrown(CarException)
-    }
-
-    def 'null begin'() {
-        when:
-        new Renting(DRIVING_LICENSE,null,date2,this.car,NIF,IBAN_BUYER)
-
-        then:
-        thrown(CarException)
-    }
-
-    def 'null end'() {
-        when:
-        new Renting(DRIVING_LICENSE,date1,null,this.car,NIF,IBAN_BUYER)
-
-        then:
-        thrown(CarException)
-    }
-
-    def 'end before begin'() {
-        when:
-        new Renting(DRIVING_LICENSE,date2,date1,this.car,NIF,IBAN_BUYER)
-
-        then:
-        thrown(CarException)
-    }
-
-    def 'null car'() {
-        when:
-        new Renting(DRIVING_LICENSE,date1,date2,null,NIF,IBAN_BUYER)
-
-        then:
-        thrown(CarException)
+        where:
+        dl   | d1 | d2 | veh | nif | iban
+        null | date1 | date2 | car | NIF | IBAN_BUYER
+        ''   | date1 | date2 | car | NIF | IBAN_BUYER
+        '12' | date1 | date2 | car | NIF | IBAN_BUYER
+        DRIVING_LICENSE | null  | date2 | car  | NIF | IBAN_BUYER
+        DRIVING_LICENSE | date1 | null  | car  | NIF | IBAN_BUYER
+        DRIVING_LICENSE | date2 | date1 | car  | NIF | IBAN_BUYER
+        DRIVING_LICENSE | date1 | date2 | null | NIF | IBAN_BUYER
     }
 }
