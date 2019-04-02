ebf3eb00053a3483692edce47239ccab204db4a6
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2019-04-02 20:37:51 +0100

    payment as transfer for car

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/remote/dataobjects/RestBankOperationData.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/remote/dataobjects/RestBankOperationData.java
@@ -1,83 +1,92 @@
 package pt.ulisboa.tecnico.softeng.car.services.remote.dataobjects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.joda.time.DateTime;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public class RestBankOperationData {
-	private String reference;
-	private String type;
-	private String iban;
-	private Double value;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
-	private DateTime time;
-	private String transactionSource;
-	private String transactionReference;
+    private String reference;
+    private String type;
+    private String sourceIban;
+    private String targetIban;
+    private long value;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+    private DateTime time;
+    private String transactionSource;
+    private String transactionReference;
 
-	public RestBankOperationData() {
-	}
+    public RestBankOperationData() {
+    }
 
-	public RestBankOperationData(String iban, double value, String transactionSource, String transactionReference) {
-		this.iban = iban;
-		this.value = value;
-		this.transactionSource = transactionSource;
-		this.transactionReference = transactionReference;
-	}
+    public RestBankOperationData(String sourceIban, String targetIban, long value, String transactionSource, String transactionReference) {
+        this.sourceIban = sourceIban;
+        this.targetIban = targetIban;
+        this.value = value;
+        this.transactionSource = transactionSource;
+        this.transactionReference = transactionReference;
+    }
 
-	public String getReference() {
-		return this.reference;
-	}
+    public String getReference() {
+        return this.reference;
+    }
 
-	public void setReference(String reference) {
-		this.reference = reference;
-	}
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
 
-	public String getType() {
-		return this.type;
-	}
+    public String getType() {
+        return this.type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public String getIban() {
-		return this.iban;
-	}
+    public long getValue() {
+        return this.value;
+    }
 
-	public void setIban(String iban) {
-		this.iban = iban;
-	}
+    public void setValue(long value) {
+        this.value = value;
+    }
 
-	public Double getValue() {
-		return this.value;
-	}
+    public DateTime getTime() {
+        return this.time;
+    }
 
-	public void setValue(Double value) {
-		this.value = value;
-	}
+    public void setTime(DateTime time) {
+        this.time = time;
+    }
 
-	public DateTime getTime() {
-		return this.time;
-	}
+    public String getTransactionSource() {
+        return this.transactionSource;
+    }
 
-	public void setTime(DateTime time) {
-		this.time = time;
-	}
+    public void setTransactionSource(String transactionSource) {
+        this.transactionSource = transactionSource;
+    }
 
-	public String getTransactionSource() {
-		return this.transactionSource;
-	}
+    public String getTransactionReference() {
+        return this.transactionReference;
+    }
 
-	public void setTransactionSource(String transactionSource) {
-		this.transactionSource = transactionSource;
-	}
+    public void setTransactionReference(String transactionReference) {
+        this.transactionReference = transactionReference;
+    }
 
-	public String getTransactionReference() {
-		return this.transactionReference;
-	}
+    public String getSourceIban() {
+        return this.sourceIban;
+    }
 
-	public void setTransactionReference(String transactionReference) {
-		this.transactionReference = transactionReference;
-	}
+    public void setSourceIban(String sourceIban) {
+        this.sourceIban = sourceIban;
+    }
+
+    public String getTargetIban() {
+        return this.targetIban;
+    }
+
+    public void setTargetIban(String targetIban) {
+        this.targetIban = targetIban;
+    }
 
 }
